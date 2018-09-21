dc52946214cf45680a403fff974662b806d9ee87
@d.rozlomiy <d.rozlomiy@bioinfo.pl>
2018-09-21 10:52:17 +0200

    refactoring

--- pico_framework/utils.py
+++ pico_framework/utils.py
@@ -9,13 +9,8 @@ def get_stats_price(pairs: list):
     """
     result = []
     for pair in pairs:
-        try:
-            qeuryset = models.CurrentMarketPrice.objects.filter(
-                unit_id=pair[1], stock_id=pair[0])
-        except models.CurrentMarketPrice.DoesNotExist:
-            pass
-        else:
-            result.append(sers.CurrentMarketPriceSerializer(
-                qeuryset, many=True).data)
-
+        qeuryset = models.CurrentMarketPrice.objects.filter(
+            unit_id=pair[1], stock_id=pair[0])
+        result.append(sers.CurrentMarketPriceSerializer(
+            qeuryset, many=True).data)
     return result
