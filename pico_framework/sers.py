450810d47c160bb18bf3c76b2a3a68dcf139c5ca
Krzysztof Kuchta <kkuchta@togetherdata.com>
2018-09-21 13:04:36 +0200

    Displaying codes.

--- pico_framework/sers.py
+++ pico_framework/sers.py
@@ -3,6 +3,19 @@ from pico_framework.models import CurrentMarketPrice
 
 
 class CurrentMarketPriceSerializer(serializers.ModelSerializer):
+    market = serializers.SerializerMethodField()
+    stock_code = serializers.SerializerMethodField()
+    unit_code = serializers.SerializerMethodField()
+
     class Meta:
         model = CurrentMarketPrice
         fields = '__all__'
+
+    def get_market(self, obj):
+        return "%s/%s" %(obj.get_stock_id_display(), obj.get_unit_id_display())
+
+    def get_stock_code(self, obj):
+        return obj.get_stock_id_display()
+
+    def get_unit_code(self):
+        return obj.get_unit_id_display()
