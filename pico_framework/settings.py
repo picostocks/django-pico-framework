c96ec23fce6981ef57bef14fffcbe855acb5ad69
Krzysztof Kuchta <kkuchta@togetherdata.com>
2018-10-03 14:30:25 +0200

    Deleting celery app.

--- pico_framework/settings.py
+++ pico_framework/settings.py
@@ -13,3 +13,16 @@ DEFAULTS = {
 def get_settings(name):
     framework_settings = getattr(settings, FRAMEWORK_SETTINGS_NAME, DEFAULTS)
     return framework_settings.get(name, DEFAULTS.get(name))
+
+
+def configure_celery(celery_app):
+    celery_app.conf.beat_schedule['pico_fwm_sync_stats'] = {
+        'task': 'pico_framework.tasks.sync_stats_task',
+        'schedule': get_settings('SYNC_STATS_EVERY')
+    }
+
+    celery_app.beat_schedule['pico_fwm_sync_current_price_task'] = {
+        'task': 'pico_framework.tasks.sync_current_price_task',
+        'schedule': get_settings('SYNC_PRICE_EVERY')
+
+    }
